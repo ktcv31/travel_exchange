b5405fc2303cb555e765e934aa9b056062fdb670
ktcv31 <163956324+ktcv31@users.noreply.github.com>
2024-09-26 18:00:47 -0500

    Update Login.js

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
       if (response.ok) {
         localStorage.setItem('token', data.token);
         alert('Login successful');
-        window.location.href = '/explore';
+        window.location.href = '/Explore';
       } else {
         setError('Login failed: ' + data.message);
       }
