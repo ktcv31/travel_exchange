9bc4564e7bc6daeba01aea370f0f42d8d838b2b7
ktcv31 <163956324+ktcv31@users.noreply.github.com>
2024-09-26 11:25:23 -0500

    Update mapsController.js

--- backend/controllers/mapsController.js
+++ backend/controllers/mapsController.js
@@ -1,6 +1,6 @@
 exports.getMapByLocation = (req, res) => {
     const { city, country } = req.query;
-    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;;
   
     const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${city},${country}`;
     res.status(200).json({ success: true, mapUrl: mapUrl });
